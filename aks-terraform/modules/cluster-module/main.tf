c6713636f65ddba8a4240502cd1605d09fe1a20d
danilenzo <81696550+danilenzo@users.noreply.github.com>
2024-01-25 23:19:35 +0000

    Update main.tf

--- aks-terraform/modules/cluster-module/main.tf
+++ aks-terraform/modules/cluster-module/main.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "azurerm" {
   features         {}
-  client_id       = "0d853e5d-1398-4958-b9a7-bb8448d47bd5"
-  client_secret   = "Kht8Q~p2ftEMIzjNGfyh.F764WSdor~gnSDPvcEu"
-  subscription_id = "e64a8fc7-dc70-4104-8933-7664dab62c2a"
-  tenant_id       = "47d4542c-f112-47f4-92c7-a838d8a5e8ef"
+  client_id       = ""
+  client_secret   = ""
+  subscription_id = ""
+  tenant_id       = ""
 }
 
 module "networking" {
@@ -33,8 +33,8 @@ module "aks_cluster" {
   cluster_location           = "UK South"
   dns_prefix                 = "myaks-project"
   kubernetes_version         = "1.26.6"  # Adjust the version as needed
-  service_principal_client_id = "0d853e5d-1398-4958-b9a7-bb8448d47bd5"
-  service_principal_client_secret = "Kht8Q~p2ftEMIzjNGfyh.F764WSdor~gnSDPvcEu"
+  service_principal_client_id = ""
+  service_principal_client_secret = ""
 
   # Input variables referencing outputs from the networking module
   resource_group_name         = module.networking.network_resource_group_name
