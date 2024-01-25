49a72c70b17909ed595e6db4c8577f46171e4501
Danil Vorobjov <danilvorobjov@hotmail.co.uk>
2024-01-25 13:55:33 +0000

    Latest version of the project

--- aks-terraform/main.tf
+++ aks-terraform/main.tf
@@ -16,13 +16,13 @@ provider "azurerm" {
 }
 
 module "networking" {
-  source              = "./modules/networking-module"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  virtual_network_name = var.virtual_network_name
-  virtual_network_address_space = var.virtual_network_address_space
-  subnet_name          = var.subnet_name
-  subnet_address_prefixes     = var.subnet_address_prefixes
+  source = "./networking-module"
+
+  # Input variables for the networking module
+  resource_group_name = "networking-rg" # Creating the resource group name
+  location           = "UK South" # Location of the Cluster
+  vnet_address_space = ["10.0.0.0/16"] # The address space specifies the range of IP addresses that can be used within the virtual network.
+
 }
 
 module "aks_cluster" {
@@ -33,8 +33,8 @@ module "aks_cluster" {
   cluster_location           = "UK South"
   dns_prefix                 = "myaks-project"
   kubernetes_version         = "1.26.6"  # Adjust the version as needed
-  service_principal_client_id = "" # Enter your Client ID
-  service_principal_client_secret = "" # Enter your password for your client ID
+  service_principal_client_id = "0d853e5d-1398-4958-b9a7-bb8448d47bd5" # Enter your Client ID
+  service_principal_client_secret = "Kht8Q~p2ftEMIzjNGfyh.F764WSdor~gnSDPvcEu" # Enter your password for your client ID
 
   # Input variables referencing outputs from the networking module
   resource_group_name         = module.networking.network_resource_group_name
